01f505d308ed718f819b129fc990c8ad2f6e04b6
Joseph Schuchart <schuchart@icl.utk.edu>
2023-04-25 16:56:32 -0400

    Add OSU bencharks, osu_reduce currently ported to MPIX_Harmonize
    Signed-off-by: Joseph Schuchart <schuchart@icl.utk.edu>

---
+++ benchmarks/osu-micro-benchmarks-7.1-1-harmonize/env.sh
@@ -0,0 +1,3 @@
+export REPROMPI_LIB_PARAMS="--clock-sync=Topo2 --params=topoalg1:hca3offset@skampi_offset@5@20 --params=topoalg2:prop@0"
+export LD_LIBRARY_PATH=/sw/hawk-rh8/hlrs/spack/rev-009_2022-09-01/gsl/2.6-gcc-10.2.0-qloqbuuq/lib:/zhome/academic/HLRS/hlrs/hpcjschu/opt-hawk/mpix-harmonize/lib64:$LD_LIBRARY_PATH
+#../configure CC=scorep-mpicc CXX=scorep-mpicxx CFLAGS="-I/zhome/academic/HLRS/hlrs/hpcjschu/opt-hawk/mpix-harmonize/include/" LDFLAGS="-L/zhome/academic/HLRS/hlrs/hpcjschu/opt-hawk/mpix-harmonize/lib64 -lmpix-harmonize -lreproMPIbench -lsynclib -L/sw/hawk-rh8/hlrs/spack/rev-009_2022-09-01/gsl/2.6-gcc-10.2.0-qloqbuuq/lib -lgsl -lgslcblas"
