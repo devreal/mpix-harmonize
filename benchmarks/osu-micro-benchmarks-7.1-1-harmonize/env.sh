43b1dccf4a74336716cf399e5a1f18d52b410998
Sascha Hunold <sascha@hunoldscience.net>
2023-10-10 22:35:44 +0200

    updated env.sh and did autoreconf

--- benchmarks/osu-micro-benchmarks-7.1-1-harmonize/env.sh
+++ benchmarks/osu-micro-benchmarks-7.1-1-harmonize/env.sh
@@ -1,3 +1,17 @@
-export REPROMPI_LIB_PARAMS="--clock-sync=Topo2 --params=topoalg1:hca3offset@skampi_offset@5@20 --params=topoalg2:prop@0"
-export LD_LIBRARY_PATH=/sw/hawk-rh8/hlrs/spack/rev-009_2022-09-01/gsl/2.6-gcc-10.2.0-qloqbuuq/lib:/zhome/academic/HLRS/hlrs/hpcjschu/opt-hawk/mpix-harmonize/lib64:$LD_LIBRARY_PATH
+
+#export REPROMPI_LIB_PARAMS="--clock-sync=Topo2 --params=topoalg1:hca3offset@skampi_offset@5@20 --params=topoalg2:prop@0"
+# export LD_LIBRARY_PATH=/sw/hawk-rh8/hlrs/spack/rev-009_2022-09-01/gsl/2.6-gcc-10.2.0-qloqbuuq/lib:/zhome/academic/HLRS/hlrs/hpcjschu/opt-hawk/mpix-harmonize/lib64:$LD_LIBRARY_PATH
+
+#export LD_LIBRARY_PATH=/sw/hawk-rh8/hlrs/spack/rev-009_2022-09-01/gsl/2.6-gcc-10.2.0-qloqbuuq/lib:/zhome/academic/HLRS/hlrs/hpcjschu/opt-hawk/mpix-harmonize/lib64:$LD_LIBRARY_PATH
 #../configure CC=scorep-mpicc CXX=scorep-mpicxx CFLAGS="-I/zhome/academic/HLRS/hlrs/hpcjschu/opt-hawk/mpix-harmonize/include/" LDFLAGS="-L/zhome/academic/HLRS/hlrs/hpcjschu/opt-hawk/mpix-harmonize/lib64 -lmpix-harmonize -lreproMPIbench -lsynclib -L/sw/hawk-rh8/hlrs/spack/rev-009_2022-09-01/gsl/2.6-gcc-10.2.0-qloqbuuq/lib -lgsl -lgslcblas"
+
+#export MPITS_PARAMS="--clock-sync=Topo2 --params=topoalg1:hca3offset@skampi_offset@5@20 --params=topoalg2:prop@0"
+export MPITS_PARAMS="--clock-sync=HCA3O --params=alg:hca3offset@skampi_offset@5@20"
+
+# build like so
+# export MPIX_PATH=$HOME/<MYPATH>/mpix-harmonize
+# export MPIX_PATH="$(pwd)/../.."
+# ./configure CC=mpicc CXX=mpicxx CFLAGS="-I${MPIX_PATH}" LDFLAGS="-L${MPIX_PATH} -lmpix-harmonize"
+# make
+# export LD_LIBRARY_PATH=$MPIX_PATH:$LD_LIBRARY_PATH
+# mpirun -np 16 ./c/mpi/collective/blocking/osu_reduce -f
